d9363bf2d70d61706858a175d337cdade5f645f8
Chih-Hao Liu <68186835+tomohiroliu22@users.noreply.github.com>
2023-10-15 16:59:23 +0800

    Create curr_time.h

---
+++ curr_time.h
@@ -0,0 +1,10 @@
+/* curr_time.h
+
+   Header file for curr_time.c.
+*/
+#ifndef CURR_TIME_H
+#define CURR_TIME_H             /* Prevent accidental double inclusion */
+
+char *currTime(const char *fmt);
+
+#endif
